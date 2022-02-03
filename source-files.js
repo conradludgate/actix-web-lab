6c186875387f182122f6ab45353bdd62471dd04f
robjtede <robjtede@icloud.com>
2022-02-03 06:59:24 +0000

    Deploying to rustdoc-gh-pages from @ robjtede/actix-web-lab@1df821940aa685ca6fe36b2b830a98723099d7e5 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["actix_web_lab"] = {"name":"","files":["acceptable.rs","body.rs","buffered_serializing_stream.rs","channel_body.rs","csv.rs","display_stream.rs","extract.rs","guard.rs","html.rs","json.rs","lazy_data.rs","lib.rs","middleware.rs","middleware_from_fn.rs","ndjson.rs","query.rs","redirect.rs","redirect_to_https.rs","redirect_to_www.rs","respond.rs","spa.rs","utils.rs","web.rs"]};
+sourcesIndex["actix_web_lab"] = {"name":"","files":["acceptable.rs","body.rs","body_hash.rs","buffered_serializing_stream.rs","channel_body.rs","csv.rs","display_stream.rs","extract.rs","guard.rs","html.rs","json.rs","lazy_data.rs","lib.rs","middleware.rs","middleware_from_fn.rs","ndjson.rs","query.rs","redirect.rs","redirect_to_https.rs","redirect_to_www.rs","respond.rs","spa.rs","utils.rs","web.rs"]};
 createSourceSidebar();
